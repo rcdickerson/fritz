66a6a81c04d4ee5bfb07b34d161c5e72f5ec5da5
Rob Dickerson <rc.dickerson@gmail.com>
2020-05-08 15:57:42 -0400

    Adding datasets and results for synonym / antonym replacement.

--- perturb.py
+++ perturb.py
@@ -1,5 +1,8 @@
 import argparse
 import csv
+from nltk import download as nltk_download
+from nltk import Tree
+from nltk.corpus import wordnet
 import random
 from tqdm import tqdm
 
@@ -12,36 +15,101 @@ def parse_args():
     parser.add_argument('-o', '--output', type=str, default='out.tsv',
                         help='file path to output perturbed data')
     parser.add_argument('-t', '--type', type=str, dest='ptype', default='shuffle',
-                        choices=['shuffle', 'neghyp'],
+                        choices=['shuffle', 'neghyp', 'hverbsyn', 'hverbant',
+                                 'hadvsyn', 'hadvant', 'hnounsyn', 'hnounant'],
                         help='the type of perturbation to perform, default is swap')
     return parser.parse_args()
 
+
+def rand_syn(word, pos):
+    syns = [lemma.name()
+            for syn in wordnet.synsets(word, pos=pos)
+            for lemma in syn.lemmas()]
+    return random.choice(syns).replace('_', ' ') if len(syns) > 0 else word
+
+def rand_ant(word, pos):
+    ants = [ant.name()
+            for syn in wordnet.synsets(word, pos)
+            for lemma in syn.lemmas()
+            for ant in lemma.antonyms()]
+    return random.choice(ants).replace('_', ' ') if len(ants) > 0 else word
+
+def transform_sentence(parse, pos_prefix, transform_func):
+    transform = []
+    any_replacements = False
+    for (word, pos) in parse:
+        if pos.startswith(pos_prefix):
+            repl_word = transform_func(word)
+            if word != repl_word: any_replacements = True
+            transform.append(repl_word)
+        else:
+            transform.append(word)
+    return (any_replacements, fix_contractions(' '.join(transform)))
+
+# The MNLI parse breaks contractions up as separate words,
+# this is a helper method to put them back together.
+def fix_contractions(sentence):
+    return sentence.replace(" '", "'").replace(" n'", "n'")
 
 def perturb(ptype, row):
     premise = row['sentence1']
     hypothesis = row['sentence2']
     is_entailment = row['gold_label'] == 'entailment'
     is_neutral = row['gold_label'] == 'neutral'
-
-    (pwords, hwords) = (premise.split(), hypothesis.split())
+    perturbed = False
 
     if ptype == 'shuffle':
+        (pwords, hwords) = (premise.split(), hypothesis.split())
         random.shuffle(pwords)
         random.shuffle(hwords)
         premise = ' '.join(pwords)
         hypothesis = ' '.join(hwords)
         is_entailment = False
+        perturbed = True
+
     elif ptype == 'neghyp':
         hypothesis = 'It is not the case that ' + hypothesis[0].lower() + hypothesis[1:]
         is_entailment = False if is_neutral else not is_entailment
+        perturbed = True
 
-    return {'gold_label': 'entailment' if is_entailment else 'non-entailment',
+    elif ptype == 'hverbsyn':
+        hparse = Tree.fromstring(row['sentence2_parse']).pos()
+        (perturbed, hypothesis) = transform_sentence(hparse, 'VB', lambda word: rand_syn(word, 'v'))
+
+    elif ptype == 'hverbant':
+        hparse = Tree.fromstring(row['sentence2_parse']).pos()
+        (perturbed, hypothesis) = transform_sentence(hparse, 'VB', lambda word: rand_ant(word, 'v'))
+        if perturbed:
+            is_entailment = False if is_neutral else not is_entailment
+
+    elif ptype == 'hadvsyn':
+        hparse = Tree.fromstring(row['sentence2_parse']).pos()
+        (perturbed, hypothesis) = transform_sentence(hparse, 'RB', lambda word: rand_syn(word, 'r'))
+    elif ptype == 'hadvant':
+        hparse = Tree.fromstring(row['sentence2_parse']).pos()
+        (perturbed, hypothesis) = transform_sentence(hparse, 'RB', lambda word: rand_ant(word, 'r'))
+        if perturbed:
+            is_entailment = False if is_neutral else not is_entailment
+
+    elif ptype == 'hnounsyn':
+        hparse = Tree.fromstring(row['sentence2_parse']).pos()
+        (perturbed, hypothesis) = transform_sentence(hparse, 'NN', lambda word: rand_syn(word, 'n'))
+    elif ptype == 'hnounant':
+        hparse = Tree.fromstring(row['sentence2_parse']).pos()
+        (perturbed, hypothesis) = transform_sentence(hparse, 'NN', lambda word: rand_ant(word, 'n'))
+        if perturbed:
+            is_entailment = False if is_neutral else not is_entailment
+
+
+    return (perturbed,
+            {'gold_label': 'entailment' if is_entailment else 'non-entailment',
             'sentence1' : premise,
-            'sentence2' : hypothesis}
+            'sentence2' : hypothesis})
 
 
 def main():
     args = parse_args()
+    nltk_download('wordnet')
 
     out_fieldnames = ['gold_label', 'sentence1', 'sentence2']
 
@@ -50,7 +118,8 @@ def main():
         tsv_writer = csv.DictWriter(outfile, fieldnames=out_fieldnames, delimiter='\t')
         tsv_writer.writeheader()
         for row in tqdm(tsv_reader):
-            tsv_writer.writerow(perturb(args.ptype, row))
+            (perturbed, perturbation) = perturb(args.ptype, row)
+            if perturbed: tsv_writer.writerow(perturbation)
 
 
 if __name__ == '__main__':
